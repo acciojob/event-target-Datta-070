9ddeb06a951aaa49038f5d47175a169cc8fce7b1
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-01-08 08:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,49 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
-  
-    removeEventListener(event, callback) {
-      
+
+    const eventListeners = this.listeners.get(event);
+    if (!eventListeners.includes(callback)) {
+      eventListeners.push(callback);
     }
-  
-    dispatchEvent(event) {
-    
-    }
-    
   }
+
+  removeEventListener(event, callback) {
+    if (this.listeners.has(event)) {
+      const eventListeners = this.listeners.get(event);
+      this.listeners.set(
+        event,
+        eventListeners.filter((listener) => listener !== callback)
+      );
+    }
+  }
+
+  dispatchEvent(event) {
+    const eventListeners = this.listeners.get(event);
+    if (eventListeners) {
+      for (const listener of eventListeners) {
+        listener();
+      }
+    }
+  }
+}
+
+// Sample Usage
+const target = new EventTarget();
+const logHello = () => console.log('hello');
+const logWorld = () => console.log('world');
+
+target.addEventListener('hello', logHello);
+target.addEventListener('world', logWorld);
+
+target.dispatchEvent('hello'); // Console logs: hello
+target.dispatchEvent('world'); // Console logs: world
+
+target.removeEventListener('hello', logHello);
+
+target.dispatchEvent('hello'); // No console log, as the listener is removed
+target.dispatchEvent('world'); // Console logs: world
   
